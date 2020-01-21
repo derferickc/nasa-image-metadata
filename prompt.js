f5dc8e37eec6f7856d23f945a69d3c1cd023da90
Frederick Choe <frederick@glamhive.com>
2020-01-21 13:47:55 -0800

    first commit

---
+++ prompt.js
@@ -0,0 +1,17 @@
+### RareCyte Front End Developer Coding Challenge
+
+# Reactive Client for NASA Images
+
+Implement and submit a small single page app web client against the NASA Image and Video Library:
+https://images.nasa.gov/ (All NASA APIs: https://api.nasa.gov/, 
+Image and Video Library APIs documentation: https://images.nasa.gov/docs/images.nasa.gov_api_docs.pdf)
+
+Your client should use a reactive modern Javascript framework (we use Vue.js at RareCyte but 
+its not a requirement for this challenge). 
+
+Your client should display a table of entries and details for a 
+selected entry from the table. Details for every entry should include at least one image and 
+some of its metadata. 
+
+Your solution should include a github link to your web client along with instructions
+on how to install, test and run it. Send the link to pvarshavskaya@rarecyte.com 
